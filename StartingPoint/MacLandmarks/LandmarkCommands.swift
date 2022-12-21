d4a4737233509faf78fd27d6ef516bdba8e66850
Steve H <a.king.life@pm.me>
2022-12-21 16:59:52 -0500

    built in menu command

---
+++ StartingPoint/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkCommands.swift
+//  CreatingAwatchOSApp
+//
+//  Created by Stephen Handy on 12/21/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
