dc555125279d8b3d6c246360dc27f7768761ed48
Stephen Handy <bolds-slicks0g@icloud.com>
2024-04-30 10:47:04 -0400

    create row view

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Steve Handy on 2024.04.30.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0]) // instantiate landmark var
+}
