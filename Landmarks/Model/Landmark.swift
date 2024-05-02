e62d5605d520b69bc56f23736b84438baf5ec6c4
Stephen Handy <bolds-slicks0g@icloud.com>
2024-05-02 09:01:11 -0400

    mark favorite landmarks

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -15,6 +15,7 @@ struct Landmark: Hashable, Codable, Identifiable { // Codable to move data betwe
     var park: String
     var state: String
     var description: String
+    var isFavorite: Bool
     
     private var imageName: String
     var image: Image {
