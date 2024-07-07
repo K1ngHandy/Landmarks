42f22383174e8842dcdfe36341741cfcdf859a73
Stephen Handy <bolds-slicks0g@icloud.com>
2024-07-07 10:20:40 -0400

    add built-in menu command

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Steve Handy on 2024.07.07.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
